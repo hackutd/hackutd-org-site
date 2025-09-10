0bd0d39f397fe8a8e2952425c78556c8b0363516
Rayyan Waris <raywa04@gmail.com>
2025-09-09 21:00:04 -0500

    fix: Update tab

--- pages/index.js
+++ pages/index.js
@@ -6,10 +6,10 @@ export default function Home() {
   return (
     <>
     <Head>
-      <title>HackUTD | Texas&apos; Largest University Hackathon</title>
+      <title>HackUTD | North America&apos;s Largest 24 Hour University Hackathon</title>
       <meta 
         name="description" 
-        content="We plan and host HackUTD, Texas&apos; largest university-run hackathon. We also assist other hackathons as part of our greater endeavor to use hacking to bring people together to learn new things."
+        content="We plan and host HackUTD, North America&apos;s largest 24 hour university-run hackathon. We also assist other hackathons as part of our greater endeavor to use hacking to bring people together to learn new things."
       />
       
       {/* Primarily for Apple iMessage previews. OG only works with an absolute path. 
